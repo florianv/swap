6ee2589a8665f093323516dee3842b2784794a6a
Petr Kramar <kramar@masquerade.cz>
2016-04-08 01:25:22 +0200

    Format library according PSR-2

--- src/Provider/CentralBankOfCzechRepublicProvider.php
+++ src/Provider/CentralBankOfCzechRepublicProvider.php
@@ -26,7 +26,7 @@ use Swap\Util\StringUtil;
 class CentralBankOfCzechRepublicProvider extends AbstractProvider
 {
     const URL = 'http://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.txt';
-	const DATE_FORMAT = 'd.m.Y';
+    const DATE_FORMAT = 'd.m.Y';
 
     /**
      * {@inheritdoc}
@@ -39,31 +39,31 @@ class CentralBankOfCzechRepublicProvider extends AbstractProvider
             throw new UnsupportedCurrencyPairException($currencyPair);
         }
 
-		$lines = explode("\n", $content);
+        $lines = explode("\n", $content);
 
-		$date = \DateTime::createFromFormat(self::DATE_FORMAT, $this->parseDate($lines[0]));
-		$date->setTime(0, 0, 0); // set hours, minutes, seconds to midnight
+        $date = \DateTime::createFromFormat(self::DATE_FORMAT, $this->parseDate($lines[0]));
+        $date->setTime(0, 0, 0); // set hours, minutes, seconds to midnight
 
-		foreach (array_slice($lines, 2) as $currency) { // skip first two lines (date and headers)
-			list(,, $count, $code, $rate) = explode('|', $currency);
-			if ($code === $currencyPair->getBaseCurrency()) {
-				$rate = (float) str_replace(',', '.', $rate);
-				return new Rate((string) ($rate / (int) $count), $date);
-			}
-		}
+        foreach (array_slice($lines, 2) as $currency) { // skip first two lines (date and headers)
+            list(,, $count, $code, $rate) = explode('|', $currency);
+            if ($code === $currencyPair->getBaseCurrency()) {
+                $rate = (float) str_replace(',', '.', $rate);
+                return new Rate((string) ($rate / (int) $count), $date);
+            }
+        }
 
         throw new UnsupportedCurrencyPairException($currencyPair);
     }
 
-	/**
-	 * Parse date from export
-	 *
-	 * @param string first line of fetched response
-	 * @return string date
-	 */
-	private function parseDate($line)
-	{
-		$words = preg_split('/[\s]+/', $line);
-		return $words[0];
-	}
+    /**
+     * Parse date from export
+     *
+     * @param string first line of fetched response
+     * @return string date
+     */
+    private function parseDate($line)
+    {
+        $words = preg_split('/[\s]+/', $line);
+        return $words[0];
+    }
 }
